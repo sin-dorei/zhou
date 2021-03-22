2385754851912dbd43173426f071f54bcb99ce96
sin-dorei <228827120@qq.com>
2021-03-22 09:16:00 +0000

    填充用户数据

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -5,18 +5,14 @@ use App\Models\User;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
     public function run()
     {
-        factory(User::class)->times(20)->create();
+        factory(User::class)->times(15)->create();
 
         $user = User::first();
         $user->name = 'Summer';
         $user->email = 'summer@emt.com';
+        $user->avatar = 'https://cdn.learnku.com/uploads/images/201710/14/1/ZqM7iaP4CR.png';
         $user->save();
     }
 }
